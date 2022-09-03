62c3f963a7eb264c76dd2c177a7bfd4573675b5a
pooja pincha <poojapincha88@gmail.com>
2022-09-03 17:35:25 +0530

    updated files

--- Progressbar/index.js
+++ Progressbar/index.js
@@ -1,5 +1,10 @@
-var progress=document.getElementById('progress');
-var name_input=document.getElementsByClassName9('nameinput');
-name_input.addEventListener(onkeyup,function(){
+console.log('hello world');
+
+var i = document.querySelector('.nameinput');
+function charCount(input){
+    var width=0;
+    var l=input.value.length;
+    var p=document.querySelector('.progress-bar');
+    p.style.width=l+"%";
     
-})
+}
